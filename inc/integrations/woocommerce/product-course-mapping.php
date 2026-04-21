596e955f7dd5dd36fe938044ce1abec9f212b88c
Adithyudu Bhaskaruni <aditechkb@gmail.com>
2026-04-21 08:25:25 +0530

    updated the product course mapping table definition

--- inc/integrations/woocommerce/product-course-mapping.php
+++ inc/integrations/woocommerce/product-course-mapping.php
@@ -29,17 +29,19 @@ class BKS_Product_Course_Mapping {
     public static function maybe_create_table() {
         global $wpdb;
 
+        $table = $wpdb->prefix . 'bks_product_course_mapping';
         $charset_collate = $wpdb->get_charset_collate();
 
-        $sql = "CREATE TABLE IF NOT EXISTS " . self::$table . " (
+        $sql = "CREATE TABLE $table (
             id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-            product_id BIGINT UNSIGNED NOT NULL UNIQUE,
-            course_id BIGINT UNSIGNED NOT NULL UNIQUE,
+            product_id BIGINT UNSIGNED NOT NULL,
+            course_id BIGINT UNSIGNED NOT NULL,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
             PRIMARY KEY (id),
-            KEY product_id (product_id),
-            KEY course_id (course_id)
+            UNIQUE KEY uniq_product (product_id),
+            UNIQUE KEY uniq_course (course_id)
         ) $charset_collate;";
 
         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
